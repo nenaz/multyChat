8c2af4cf100e184f1bc1698de8c04f43c34a7fe8
mrnenaz <mrnenaz@gmail.com>
2019-07-21 14:00:43 +0300

    go to new repository, reload app

--- app.js
+++ app.js
@@ -1,192 +1,103 @@
-// var express = require('express');
-// var app = express();
-// var bodyParser = require('body-parser');
-var cors = require('cors');
-// var utilsurl = require('url');
-// var VK = require('vk-io');
-// var proxy = require('http-proxy-middleware');
+const fs = require('fs');
+const path = require('path')
+const http = require('http');
+const https = require('https');
+const express = require('express')
+const urlencoded = require("body-parser")
+const json = require("body-parser")
+const app = express();
+const url = require('url');
+const certOptions = {
+  key: fs.readFileSync(path.resolve('certs/ca.key')),
+  cert: fs.readFileSync(path.resolve('certs/ca.crt'))
+}
+const cors = require('cors')
 
-// var vk = new VK({
-//     app: 6079803,
-//     // login: VK_LOGIN, //string
-//     // pass: VK_PASSWORD, //string
-// });
-// app.use('/*', cors(), bodyParser.json(), proxy({
-//     target: target,
-//     changeOrigin: true,
-//     logLevel: logLevel,
-//     router: appRouter
-// }));
+const CLIENT_ORIGIN = [
+  'https://127.0.0.1:3000',
+  'https://localhost:3000',
+  'https://localhost:8080',
+  'https://127.0.0.1:8080',
+  'http://localhost:3000',
+  'http://localhost:3001',
+]
 
-// var userObject = {
-//     gltoken: '',
-//     app: 6079803,
-//     // login: VK_LOGIN,
-//     // pass: VK_PASSWORD,
-//     userid: '',
-//     dialogUserId: '',
-//     friends: {}
-// };
+app.use(urlencoded({ extended: true }));
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
+  res.setHeader("Access-Control-Allow-Headers", "Origin, Accept, Content-Type, Authorization, Access-Control-Allow-Origin");
+  next();
+});
+app.use(json());
+app.use(express.json())
 
-// function vkAuth(params) {
-//     console.log('vkAuth');
-//     console.log(params.login);
-//     console.log(params.password);
-//     vk.setOptions({
-//         app: 6079803,
-//         login: params.login,
-//         pass: params.password
-//     });
-//     var auth = vk.auth.standalone();
-//     auth.run().then(function(token) {
-//         console.log('User token:', token);
-//         setUserObject('gltoken', token);
-//         setUserObject('login', params.login);
-//         setUserObject('pass', params.password);
-//         vk.setOptions({ token: userObject.gltoken });
-//         startLongPoll();
-//     }).catch(function(error) {
-//         console.error(error);
-//     });
-// }
+app.use(cors({
+  origin: CLIENT_ORIGIN
+})) 
 
-// function sendMessage(res, params) {
-
-//     vk.api.messages.send({
-//         // user_id: params.userid || VK_ID, //string
-//         'message': params.message
-//     });
-//     res.send('Сообщение отправлено');
-// }
-
-// function getFriends() {
-//     vk.api.friends.get({
-//         user_id: userObject.userid,
-//         fields: 'nickname',
-//         count: 10
-//     }).then(function(res) {
-//         return res;
-//     });
-// }
-
-// function setUserObject(name, value) {
-//     userObject[name] = value;
-// }
-
-// function startLongPoll() {
-//     vk.api.messages.getLongPollServer({
-//         need_pts1: 1,
-//         lp_version: 2
-//     }).then(function(res) {
-//         // console.log(res);
-//         vk.longpoll.start().then(function() {
-//             console.log('Long Poll запущен');
-//             vk.longpoll.on('message', incomingMessageHandler);
-//         }).catch(function(error) {
-//             console.error(error);
-//         });
-//     });
-// }
-
-// function incomingMessageHandler(res) {
-//     console.log(res);
-// }
-
-// app.post('/', function(req, res) {
-//     res.setHeader('Content-Type', 'application/json');
-//     var params = req.body;
-//     console.log(params.type);
-//     switch (params.type) {
-//         case 'message':
-//             sendMessage(res, params);
-//             res.send('auth');
-//             break;
-//         case 'getFriends':
-//             console.log(userObject.userid);
-//             res.send(getFriends());
-//             break;
-//         default:
-//             // vkAuth(params);
-//             res.redirect('https://oauth.vk.com/authorize?client_id=6079803&display=popup&redirect_uri=https://multychat.000webhostapp.com/&scope=friends&response_type=code&v=5.67');
-//     }
-
-//     // res.send(typeof params);
-// });
-
-// app.listen(3000);
-
-
-
-// // vk.longpoll.start().then(function() {
-// //     console.log('Long Poll запущен');
-// // }).catch(function(error) {
-// //     console.error(error);
-// // });
-
-var express = require('express');
-var app = express();
-var passport = require('passport');
-var VKontakteStrategy = require('passport-vkontakte').Strategy;
-
-// User session support middlewares. Your exact suite might vary depending on your app's needs.
-app.use(require('cookie-parser')());
-app.use(require('body-parser').urlencoded({ extended: true }));
-app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
-app.use(passport.initialize());
-app.use(passport.session());
-// app.use(cors());
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
-    next();
+app.get('/', (request, response) => {
+  console.log(`URL: ${request.url}`);
+  response.send('Hello, Server!');
 });
 
-passport.use(new VKontakteStrategy({
-        clientID: 6079803, // VK.com docs call it 'API ID', 'app_id', 'api_id', 'client_id' or 'apiId'
-        clientSecret: "FjBDVir4sjuTVKtHXmVF",
-        callbackURL: "https://multychat.000webhostapp.com/"
-    },
-    function myVerifyCallbackFn(accessToken, refreshToken, params, profile, done) {
-
-        // Now that we have user's `profile` as seen by VK, we can
-        // use it to find corresponding database records on our side.
-        // Also we have user's `params` that contains email address (if set in 
-        // scope), token lifetime, etc.
-        // Here, we have a hypothetical `User` class which does what it says.
-        User.findOrCreate({ vkontakteId: profile.id })
-            .then(function(user) { done(null, user); })
-            .catch(done);
-    }
-));
-
-// User session support for our hypothetical `user` objects.
-passport.serializeUser(function(user, done) {
-    done(null, user.id);
+app.post('/', (request, response) => {
+  console.log(`URL: ${request.url}`);
+  response.send('Hello, Server!');
 });
 
-passport.deserializeUser(function(id, done) {
-    User.findById(id)
-        .then(function(user) { done(null, user); })
-        .catch(done);
+app.get('/vkontakte/callback', (req, res) => {
+  console.log('URL----------req:', req);
+  console.log('URL----------res:', res);
+  const urlFormat = url.format({
+      protocol: req.protocol,
+      host: req.get('host')
+  });
+  console.log('urlFormat', urlFormat)
+  // const url = `https://oauth.vk.com/authorize
+  //   ?client_id=${client_id}
+  //   &display=popup
+  //   &redirect_uri=https://oauth.vk.com/blank.html
+  //   &scope=wall,friends
+  //   &response_type=token
+  //   &v=5.52
+  // `;
+  res.send(req.query);
+})
+
+app.post('/vkontakte-get', (req, res) => {
+  console.log(`URL: ${req.url}`);
+  res.send('Hello, Server!');
+  const client_id = 7022338;
+  const url = `https://oauth.vk.com/authorize
+    ?client_id=${client_id}
+    &display=popup
+    &redirect_uri=https://oauth.vk.com/blank.html
+    &scope=wall,friends
+    &response_type=token
+    &v=5.52
+  `;
+  const request = https.get(url, (response) => {
+    response.setEncoding('utf8');
+    response.on('data', (d) => {
+      console.log('---dd--', d);
+      res.end(d);
+    });
+  });
+  request.on('error', (e) => {
+    console.error(e);
+  });
+  request.end();
+  // https://oauth.vk.com/authorize?client_id=***APP_ID***&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=wall,friends&response_type=token&v=5.52
 });
 
-//This function will pass callback, scope and request new token
-app.get('/auth/vkontakte', passport.authenticate('vkontakte'));
-// app.get('/', passport.authenticate('vkontakte'));
+// const server = https.createServer(certOptions, app)
+const server = app.listen(process.env.PORT || 8080, (error) => {
+  if (error) return console.log(`Error: ${error}`);
 
-app.get('/auth/vkontakte/callback', function(req, res) {
-    // res.setHeader('Access-Control-Allow-Origin', '*');
-    passport.authenticate('vkontakte', {
-        successRedirect: '/',
-        failureRedirect: '/login'
-    })
-    passport.authenticate('vkontakte')
+  console.log(`Server listening on port ${server.address().port}`);
 });
 
-app.get('/', function(req, res) {
-    //Here you have an access to req.user
-    // res.addHeader("Access-Control-Allow-Origin", "*");
-    res.json(req.user);
-});
-
-app.listen(3000);
+// server.listen(process.env.PORT || 8080, () => {
+//   console.log('listening...')
+// })
